ea17d5d58c4499d71953bce0ebf9a2bbdfe34012
GuDahoon <iamkdh1@naver.com>
2024-01-18 12:08:00 +0900

    setup haru intro page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import MainPage from "./pages/MainPage"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <MainPage />
+    </>
+  )
+}
+
+export default App
